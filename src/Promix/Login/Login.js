aedcd57b37ccaee3c2b14f0995740679b16052b0
oluwolesuraj89 <oluwolesuraj98@gmail.com>
2024-04-25 17:13:48 +0100

    updates

--- src/Promix/Login/Login.js
+++ src/Promix/Login/Login.js
@@ -1,36 +1,36 @@
 import React from "react";
 import classes from './Login.module.css'
 import backgroundImg from '../../assets/promix/LogIn.png'
+import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function Login (){
 
 return (
 
     <div className={classes.background}>
- <div className={classes.mainContainerx}>
-                <div className={classes.thecntnt}>
-                    <div >
-                        <p className={classes.SignUptxt}>Login</p>
-                        <p className={classes.SignUptxtsub}>Login to your portal</p>
-                    </div>
-                    <div className={classes.firstset}>
-                        
-                        <div className={classes.input1}>
-                            <label className={classes.dlabl}>Email Address</label>
-                            <input placeholder="Enter your email address" type="text" className={classes.inputlbl}></input>
-                        </div>
-                    </div>
-                    <div className={classes.secondset}>
-                        <div className={classes.input1}>
-                            <label className={classes.dlabl}>Password</label>
-                            <input placeholder="Enter your password" type="password" className={classes.inputlbl}></input>
-                        </div>
-                        
-                    </div>
-                    <button className={classes.snupbtn}>Login</button>
-                    <p className={classes.lgin}>Don't have an account? <span>Sign Up</span></p>
+        <div className={classes.mainContainerx}>
+            <div className={classes.thecntnt}>
+                <div className={classes.head}>
+                    <p className={classes.SignUptxt}>Login</p>
+                    <p className={classes.SignUptxtsub}>Login to your portal</p>
                 </div>
+                <Form>
+                <Form.Group className={classes.formGroup}>
+                    <Form.Label  >Email Address</Form.Label>
+                    <Form.Control type='text' id="date" placeholder="Enter you email address"  style={{backgroundColor:'rgba(0, 0, 0, 0.8)'}}/>
+                </Form.Group>
+                <Form.Group className={classes.formGroup}>
+                    <Form.Label  >Password</Form.Label>
+                    <Form.Control type='password' id="date" placeholder="Enter you password address"  style={{backgroundColor:'rgba(0, 0, 0, 0.8)'}}/>
+                </Form.Group>
+                </Form>
+                <Button variant="success" className={classes.snupbtn}>Login</Button>
+                {/* <button className={classes.snupbtn}>Login</button> */}
+                <p className={classes.lgin}>Don't have an account? <Link to={'/signup'} style={{textDecoration:'none'}}><span>Sign Up</span></Link></p>
             </div>
+        </div>
     </div>
 );
 
