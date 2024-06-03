82995cfce823bfb59d0a7cd6ac0f305297fc6ee2
oluwolesuraj89 <oluwolesuraj98@gmail.com>
2024-06-03 11:45:03 +0100

    update

--- src/Pages/Main Dashboard/MainDashoard.js
+++ src/Pages/Main Dashboard/MainDashoard.js
@@ -55,6 +55,7 @@ export default function MainDashboard() {
     const [isMenuOpen4, setIsMenuOpen4] = useState(false);
     const [isMenuOpen5, setIsMenuOpen5] = useState(false);
     const [isMenuOpen6, setIsMenuOpen6] = useState(false);
+    const [isMenuOpen7, setIsMenuOpen7] = useState(false);
     // const { isReg, retrieveRegStatus } = useRegistration();
     const PINK = 'rgba(255, 192, 203, 0.6)';
     const BLUE = 'rgba(0, 0, 255, 0.6)';
@@ -103,9 +104,9 @@ export default function MainDashboard() {
     const toggleMenu6 = () => {
         setIsMenuOpen6(!isMenuOpen6); // Toggle the menu open/close state
     };
-    // const toggleMenu6 = () => {
-    //     setIsMenuOpen6(!isMenuOpen6); // Toggle the menu open/close state
-    // };
+    const toggleMenu7 = () => {
+        setIsMenuOpen7(!isMenuOpen7); // Toggle the menu open/close state
+    };
     
 
     const closeMenu = () => {
@@ -272,6 +273,29 @@ export default function MainDashboard() {
                                     </ContextAwareToggle>
                                 </Card.Header>
                                 <Accordion.Collapse eventKey="0" style={{backgroundColor:'#164B2E'}}>
+                                    <Card.Body className={classes.cardBody}>
+                                        <Link to={'/manage_role'}>Manage Roles</Link><br/>
+                                        <Link to={'/approval_level'}>Approval Levels</Link><br/>
+                                        <Link to={'#'}>Manage User</Link><br/>
+                                        <Link to={'#'}>Manage Category </Link><br/>
+                                        <Link to={'#'}>Charts of Account</Link><br/>
+                                        {/* <Link to={'#'}>Loan & Advances</Link> */}
+                                    </Card.Body>
+                                    
+                                </Accordion.Collapse>
+                            </Card>
+                            <Card className={classes.accordionCard}>
+                                <Card.Header className={classes.cardHeader} onClick={toggleMenu7}>
+                                    <ContextAwareToggle eventKey="7">
+                                        <p>
+                                            <span>
+                                                <img src={dIcon2} alt='icon'/> Cooperatives
+                                            </span>
+                                            {isMenuOpen ? (<i class='bx bx-chevron-down'></i>) : (<i class='bx bx-chevron-up'></i>)} 
+                                        </p>
+                                    </ContextAwareToggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="7" style={{backgroundColor:'#164B2E'}}>
                                     <Card.Body className={classes.cardBody}>
                                         <Link to={'/manage_role'}>Manage Roles</Link><br/>
                                         <Link to={'/approval_level'}>Approval Levels</Link><br/>
